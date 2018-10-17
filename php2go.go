7c0a6e2183033b22cddbbdd0bd2bcde8d4e8b930
syyong.x <syyong.x@gmail.com>
2018-10-17 14:40:09 +0800

    Update php2go.go

--- php2go.go
+++ php2go.go
@@ -1440,7 +1440,7 @@ func Fgetcsv(handle *os.File, length int, delimiter rune) ([][]string, error) {
 }
 
 // disk_free_space()
-// Only supports Unix operating systems
+// Only supports Unix/Linux operating systems
 func DiskFreeSpace(directory string) (uint64, error) {
 	fs := syscall.Statfs_t{}
 	err := syscall.Statfs(directory, &fs)
@@ -1451,7 +1451,7 @@ func DiskFreeSpace(directory string) (uint64, error) {
 }
 
 // disk_total_space()
-// Only supports Unix operating systems
+// Only supports Unix/Linux operating systems
 func DiskTotalSpace(directory string) (uint64, error) {
 	fs := syscall.Statfs_t{}
 	err := syscall.Statfs(directory, &fs)
