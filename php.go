75672e573a63faf1a4ab8e3ff24677beff6fdd23
Syyong <syyong.x@gmail.com>
2018-11-19 11:18:21 +0800

    Add comment

--- php.go
+++ php.go
@@ -1620,6 +1620,9 @@ func Empty(val interface{}) bool {
 }
 
 // is_numeric()
+// Numeric strings consist of optional sign, any number of digits, optional decimal part and optional exponential part.
+// Thus +0123.45e6 is a valid numeric value.
+// In PHP hexadecimal (e.g. 0xf4c3b00c) is not supported, but IsNumeric is supported.
 func IsNumeric(val interface{}) bool {
 	switch val.(type) {
 	case int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64:
